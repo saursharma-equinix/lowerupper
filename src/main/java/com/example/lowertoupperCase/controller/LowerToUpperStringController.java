6bfbc34a777b4c572ee5f9df3f2f3c7a112b2f5a
jagdishkumawat07 <jagdishkumawat07@gmail.com>
2023-07-13 12:44:19 +0530

    src code change

--- src/main/java/com/example/lowertoupperCase/controller/LowerToUpperStringController.java
+++ src/main/java/com/example/lowertoupperCase/controller/LowerToUpperStringController.java
@@ -9,7 +9,7 @@ public class LowerToUpperStringController {
 
 	@GetMapping("/{name}")
 	public String getString(@PathVariable String name) {
-		name = name.toUpperCase();
+		name = name.toUpperCase()+" sharma";
 		return name;
 	}
 }
